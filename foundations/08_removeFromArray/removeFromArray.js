3c1c297bdf25f1de7b90473d8d58dbd37246c274
Snehasish Mojumder <278628510+smojumder99@users.noreply.github.com>
2026-07-13 23:36:28 +0530

    Write required code to pass exercise 08 tests

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...elementsToRemove) {
+    for (let i = arr.length-1; i >= 0; i--) {
+        for (let j = 0; j < elementsToRemove.length; j++) {
+            if (arr[i] === elementsToRemove[j])
+                arr.splice(i, 1)
+        }
+    }
+    return arr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
