b97117999fa9cc0878d9caee8a83f805e1bb71e6
Snehasish Mojumder <278628510+smojumder99@users.noreply.github.com>
2026-07-14 17:19:18 +0530

    Write required code to pass exercise 11 tests

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farValue) {
+  let celValue=(farValue-32)*(5/9);
+  return Math.round(celValue*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celValue) {
+  let farValue= (celValue*(9/5))+32;
+  return Math.round(farValue*10)/10;
 };
 
 // Do not edit below this line
