80b906c343cb44614adbaa223724c0396b1432d3
Snehasish Mojumder <278628510+smojumder99@users.noreply.github.com>
2026-07-14 14:31:11 +0530

    Write required code to pass all tests of exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,55 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+
+if ((!Number.isInteger(num1) || num1 <= 0)|| ((!Number.isInteger(num2) || num2 <= 0))) {
+    return 'ERROR';
+  }
+
+
+let smallNum=num1;
+let bigNum=num2;
+
+if(num1>num2){
+    smallNum=num2;
+    bigNum=num1;
+}
+     
+let arr=[];
+let j=smallNum;
+for (let i=0;i<=bigNum-smallNum;i++){
+    arr[i]=j;
+    j++;
+}
+return arr.reduce((sum,current)=>sum+current,0);
+
+
 
 };
+
+
+// const sumAll = function(num1, num2) {
+//   if (!Number.isInteger(num1) || num1 <= 0 || !Number.isInteger(num2) || num2 <= 0) {
+//     return 'ERROR';
+//   }
+
+//   let smallNum = num1;
+//   let bigNum = num2;
+
+//   if (num1 > num2) {
+//     smallNum = num2;
+//     bigNum = num1;
+//   }
+
+//   let arr = [];
+  
+//   // Loop directly from the small number to the big number
+//   for (let i = smallNum; i <= bigNum; i++) {
+//     arr.push(i); 
+//   }
+
+//   return arr.reduce((sum, current) => sum + current, 0);
+// };
+
 
 // Do not edit below this line
 module.exports = sumAll;
