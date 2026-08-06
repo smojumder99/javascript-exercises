effc44d7973229bc0cc7d841de705a7f64a5d985
Snehasish Mojumder <278628510+smojumder99@users.noreply.github.com>
2026-08-06 20:49:12 +0530

    Pass all the tests given in the spec file

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((product, num) => product*num, 1);
 };
 
-const power = function() {
-	
+const power = function (num,power) {
+   return num**power;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+      if (n===0)
+        return 1;
+      return n* factorial(n-1);
 };
 
 // Do not edit below this line
