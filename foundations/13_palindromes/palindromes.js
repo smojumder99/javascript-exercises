d262dd1cf7210ffb516bbe1cbcf7e7822f752b1c
Snehasish Mojumder <278628510+smojumder99@users.noreply.github.com>
2026-08-07 01:46:44 +0530

    Pass all the given tests

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,47 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str=str.toUpperCase();
+    const punctuationAndSpace = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    let j=0;
+    let arr=[];
+for (let i=0;i<str.length;i++){
+    if (!(punctuationAndSpace.includes(str[i]))){
+       arr[j]=str[i];
+       j++;
+    }
+}
 
+for (let k=0,l=arr.length-1;k<l;k++,l--){
+     if(arr[k]!==arr[l])
+        return false;
+    }
+    return true;
 };
+
+
+
+// const palindromes = function (str) {
+//   // 1. Convert to lowercase to ignore capitalization
+//   str = str.toLowerCase();
+  
+//   const punctuationAndSpace = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+//   let j = 0;
+//   let arr = [];
+  
+//   for (let i = 0; i < str.length; i++) {
+//     if (!(punctuationAndSpace.includes(str[i]))) {
+//       arr[j] = str[i];
+//       j++;
+//     }
+//   }
+
+//   // 2. Change k !== l to k < l to prevent infinite loops
+//   for (let k = 0, l = arr.length - 1; k < l; k++, l--) {
+//     if (arr[k] !== arr[l]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
 // Do not edit below this line
 module.exports = palindromes;
