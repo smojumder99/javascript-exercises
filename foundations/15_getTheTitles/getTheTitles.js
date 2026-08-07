ae5677971b1eb029b1c760e2e77e5e45efede06e
Snehasish Mojumder <278628510+smojumder99@users.noreply.github.com>
2026-08-07 15:15:37 +0530

    Pass all the tests

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
 
+return arr.reduce((bookTitles,book)=>{
+    bookTitles.push(book.title)
+return bookTitles},[]);
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
