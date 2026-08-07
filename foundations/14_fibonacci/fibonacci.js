2a333058f4a2197e18fd2f1988e57732a8a8d4b0
Snehasish Mojumder <278628510+smojumder99@users.noreply.github.com>
2026-08-07 14:52:46 +0530

    Pass all the given  tests

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(fibIndex) {
+fibIndex=Number(fibIndex);
+if(fibIndex<0)
+    return "OOPS";
+if (fibIndex===0)
+    return 0;
+if ((fibIndex===1) || (fibIndex===2))
+    return 1;
+
+let arr=[];
+for (let i=1;i<=fibIndex;i++){
+    if((i===1)||(i===2)){
+        arr[i]=1;
+        continue;
+    }
+arr[i]= (arr[i-1])+(arr[i-2]);
+
+}
+return arr[arr.length-1];
 
 };
 
