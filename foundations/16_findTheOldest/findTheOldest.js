a7614060b7776119dd4dc15d3effe5f9903591d3
Snehasish Mojumder <278628510+smojumder99@users.noreply.github.com>
2026-08-08 12:03:24 +0530

    Pass all the tests

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let peopleWithAge=[];
+    for(let i=0;i<people.length;i++){
+        let currentPersonAge;
+        peopleWithAge[i]=people[i] ;
+        
+            currentPersonAge= !(people[i].yearOfDeath)? 
+            ((new Date().getFullYear())-people[i].yearOfBirth) :(people[i].yearOfDeath-people[i].yearOfBirth);
+
+            peopleWithAge[i].age=currentPersonAge;
+    }
+   peopleWithAge.sort((a,b)=>a.age-b.age);
+
+   const oldestPersonName=peopleWithAge[peopleWithAge.length-1].name;
+   return people.find(person=> person.name===oldestPersonName);
+
+    
 };
 
 // Do not edit below this line
